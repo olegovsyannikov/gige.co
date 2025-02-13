9ace417d34c195a539854673fcc6afb5e792dee0
olegovsyannikov <oleg.ovsyannikov@gmail.com>
2025-02-13 21:48:47 +0400

    + analytics, update providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { ClerkProvider } from "@clerk/nextjs";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Providers } from "@/providers/providers";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-// Create a client
-const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -16,12 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <QueryClientProvider client={queryClient}>
-    <html lang="en">
-          <body className={inter.className}>{children}</body>
-    </html>
-      </QueryClientProvider>
-    </ClerkProvider>
+    <Providers>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </Providers>
   );
 }
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import { ReactNode } from "react";
 import { AuthProvider } from "./auth-provider";
 
-// Create a client
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
+        <Analytics />
         {children}
       </QueryClientProvider>
     </AuthProvider>
